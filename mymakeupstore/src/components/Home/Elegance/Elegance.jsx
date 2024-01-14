c13409d9471c3575892bda34f6f80933c2d0baf7
Sayo Adegoroye <steve.legion94@gmail.com>
2024-01-14 12:57:41 +0100

    Update Elegance.jsx

--- mymakeupstore/src/components/Home/Elegance/Elegance.jsx
+++ mymakeupstore/src/components/Home/Elegance/Elegance.jsx
@@ -21,7 +21,7 @@ const Elegance = () => {
             Quasi eaque quibusdam placeat rerum ex voluptatibus praesentium
             porro natus iste quo impedit ipsum culpa, nulla odio error saepe.
             Totam! praesentium porro natus iste quo impedit ipsum culpa, nulla
-            odio error saepe. Totam!ghvjyqew
+            odio error saepe. ndwweed
           </p>
           <p className="font-primary text-[11px]  md:text-sm text-dark leading-7 text-justify">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
